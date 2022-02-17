b9f2bb7748bc3bdd86ca61e27fd997ad38d746a5
Khasanboy Khabibullaev <khassanboi@gmail.com>
2022-02-17 17:19:16 +0500

    update: fix nav link click close event

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 document.getElementById('menu-icon').addEventListener('click', () => {
-	console.log('working');
 	document.getElementById('nav-list').classList.toggle('visible');
+});
+
+document.querySelectorAll(".nav__item-link").forEach(el => {
+	el.addEventListener('click',function (e) {
+		document.getElementById('nav-list').classList.toggle('visible');
+	});
 });
